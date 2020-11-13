b8a0fe9a8ad3209da019dc514c42da90ad682e82
Ali Wojcikiewicz <aliwoj@Alis-MBP.cable.rcn.com>
2020-11-13 14:26:09 -0600

    created ls.js file and exported to bash.js

--- bash.js
+++ bash.js
@@ -1,4 +1,15 @@
+const pwd = require("./pwd");
 
+const ls = require("./ls");
 
-const pwd = require('./pwd');
-//pwd()</
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === "pwd") {
+    pwd();
+  } else if (cmd === "ls") {
+    ls();
+  }
+});
--- pwd.js
+++ pwd.js
@@ -1,17 +1,3 @@
+const pwdFunc = function () {};
 
-const pwdModule = function () {
-
-
-  process.stdout.write('prompt > ');
-
-  process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
-
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
-
-  })
-
-};
-
-module.exports = pwdModule;
+module.exports = pwdFunc;
